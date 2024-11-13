4ae8d8af33e18b77e81f1f08cce2b4c72da73de4
redhat@secisd.unclass.lab <redhat@secisd.unclass.lab>
2024-11-13 14:03:26 -0500

    add comments to script to explain usage

--- scripts/rhel6ify.sh
+++ scripts/rhel6ify.sh
@@ -1,2 +1,6 @@
+#!/bin/sh
+# sed commands that modify network/storage bus for vms that cannot use virtio (e.g. rhel 6)
+# to get yaml that this script modifies run `oc get vm -o yaml > vms.yaml`.
+
 sed -i -r 's/^(\s{16,})bus: virtio$/\1bus: sata/g' $1
 sed -i -r 's/(\s+)model: virtio/\1model: e1000e/g' $1
