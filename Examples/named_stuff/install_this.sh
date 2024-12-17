9ec57136f46ff2262f6b133c532d3fc367e9322f
Christopher Novak <cnovak@redhat.com>
2024-12-17 14:06:38 -0500

    Append named permission changes and firewall changes

--- Examples/named_stuff/install_this.sh
+++ Examples/named_stuff/install_this.sh
@@ -1,2 +1,8 @@
 sudo dnf install bind bind-utils
+firewall-cmd --permanent --add-service=dns
 sudo systemctl enable --now named
+
+
+chown named:named /var/named/named.zone
+chmod 640 /var/named/named.zone
+systemctl restart named
