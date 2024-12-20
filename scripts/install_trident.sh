d5974d7a208f0b6e83a406c674b315f65ac6e2a8
Christopher Novak <cnovak@redhat.com>
2024-12-20 15:32:58 -0500

    Fix default location for oc-mirror V2

--- scripts/install_trident.sh
+++ scripts/install_trident.sh
@@ -4,7 +4,7 @@ tridentctl install \
         --namespace trident \
         --image-registry 192.168.69.242:5000/sig-storage \
         --autosupport-image netapp/trident-autosupport:24.06 \
-        --trident-image netapp/trident:24.06.0
+        --trident-image 192.168.69.242:5000/trident:24.06.0
 
 
 tridentctl create backend \
